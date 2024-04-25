e31c586702c81311b064f939599349f1d8015efc
Joram Wambugu <jwambugu.dev@gmail.com>
2024-04-25 19:49:27 +0300

    feat: get todos with wait group

--- cmd/cmd/todo/main.go
+++ cmd/cmd/todo/main.go
@@ -5,6 +5,7 @@ import (
 	"go-labs/internal/dummyjson"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -18,17 +19,28 @@ func main() {
 		ctx     = context.Background()
 	)
 
-	todo, err := todoApi.Get(ctx, 1)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Printf("todo: %#+v \n", todo)
-
 	todos, err := todoApi.GetAll(ctx, 5, 2)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	log.Printf("todos: %#+v \n", todos)
+
+	var wg sync.WaitGroup
+
+	for _, t := range todos.Todos {
+		wg.Add(1)
+
+		go func(id int) {
+			defer wg.Done()
+			todo, err := todoApi.Get(ctx, id)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			log.Printf("todo: %#+v \n", todo)
+		}(t.ID)
+	}
+
+	wg.Wait()
 }
